8a2eb3b4c6018160cecc47292d9bed2c5b558425
MrKelpy <alexandresilva.coding@gmail.com>
2024-11-16 16:41:11 +0000

    Added the proper PDF

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     kotlin("jvm") version "2.0.20"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.github.iselg1"
+version = "1.0"
 
 repositories {
     mavenCentral()
